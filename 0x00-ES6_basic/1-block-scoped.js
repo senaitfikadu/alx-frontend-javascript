d4f0da7253d6b81a367ed15875c41209dbae04c2
senaitfikadu <senaitfikadu44@gmail.com>
2023-03-21 15:31:59 +0300

    add changes

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,7 +3,8 @@ export default function taskBlock(trueOrFalse) {
     const task2 = true;
   
     if (trueOrFalse) {
-      ruturn [task, task2];
+        let task = true;
+        let task2 = false;
     }
   
     return [task, task2];
